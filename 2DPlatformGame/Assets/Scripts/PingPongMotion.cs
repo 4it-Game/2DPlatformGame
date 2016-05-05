8ac59d14a83b588cc99801e1b1ee3d0fad971aae
thusitha madusanka <thusitha madusanka>
2016-05-05 12:52:18 +0530

    anim + script

---
+++ 2DPlatformGame/Assets/Scripts/PingPongMotion.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPongMotion : MonoBehaviour {
+
+	private Transform ThisTransform = null;
+
+	private Vector3 OriPos = Vector3.zero;
+
+	public Vector3 MoveAxes = Vector2.zero;
+
+	public float Distance = 3f;
+
+	// Use this for initialization
+	void Awake () {
+		ThisTransform = GetComponent<Transform> ();
+
+		OriPos = ThisTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ThisTransform.position = OriPos + MoveAxes * Mathf.PingPong (Time.time, Distance);
+	}
+}
